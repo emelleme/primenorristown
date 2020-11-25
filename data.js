908a9fca397c20eadde07a4746c53cd5237bf880
bot <git@emelle.me>
2020-11-25 09:29:14 -0500

    initial commit

---
+++ data.js
@@ -0,0 +1,313 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-edibles-and-snacks",
+      "name": "Edibles and Snacks",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.1846956809142597,
+          "pitch": 0.10117986959041403,
+          "rotation": 0,
+          "target": "1-raw-bar"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.0136779305971455,
+          "pitch": 0.04920432679754683,
+          "title": "Prime Holistic Swag",
+          "text": "<a href='https://pa.primeholistic.shop/product/tshirts/28?cp=true&sa=true&sbp=false&q=false'>Shop Now</a>"
+        },
+        {
+          "yaw": -1.6104299813798715,
+          "pitch": 0.2480759573711424,
+          "title": "Edibles",
+          "text": "<a href='https://pa.primeholistic.shop/shop/food/5'>Snacks</a> <a href='https://pa.primeholistic.shop/shop/gummies/7'>Gummies</a>"
+        },
+        {
+          "yaw": -2.5104299813798715,
+          "pitch": -0.3080759573711424,
+          "title": "Business Post Board",
+          "text": "Our Partner Businesses"
+        },
+        {
+          "yaw": -0.833114365419263,
+          "pitch": 0.24730246682789137,
+          "title": "Coffee and Teas",
+          "text": "<a href='https://pa.primeholistic.shop/shop/coffee-and-tea/8'>Shop Now</a>"
+        }
+      ]
+    },
+    {
+      "id": "1-raw-bar",
+      "name": "R.A.W. Bar",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3024585218744811,
+          "pitch": 0.10047498825789702,
+          "rotation": 0,
+          "target": "2-tinctures-flower-bath-and-body"
+        },
+        {
+          "yaw": -2.9784183557501134,
+          "pitch": 0.14120494083637425,
+          "rotation": 0,
+          "target": "0-edibles-and-snacks"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.1761411634409349,
+          "pitch": 0.21177017842931534,
+          "title": "Hemp Flower",
+          "text": "<a href='https://pa.primeholistic.shop/shop/hemp-flower/10'>Shop Now</a>"
+        },
+        {
+          "yaw": -0.7307050288446337,
+          "pitch": 0.07761561972776398,
+          "title": "Beverages",
+          "text": "<a href='https://pa.primeholistic.shop/shop/r-a-w-bar/6'>Shop Now</a>"
+        },
+        {
+          "yaw": -2.0618296679385715,
+          "pitch": 0.03966182988563283,
+          "title": "R.A.W. Bar",
+          "text": "Real. Actual. Wealth. (R.A.W.) Bar is a next generation bar that produces nutrient-rich organic drinks and shots."
+        }
+      ]
+    },
+    {
+      "id": "2-tinctures-flower-bath-and-body",
+      "name": "Tinctures, Flower, Bath and Body",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.8479670512374309,
+        "pitch": 0.0006628084009427937,
+        "fov": 1.3136650481270435
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.24839260317794043,
+          "pitch": 0.07099527259250848,
+          "rotation": 0,
+          "target": "3-pet-and-other-products"
+        },
+        {
+          "yaw": 3.0588595359933173,
+          "pitch": 0.0774536693958261,
+          "rotation": 0,
+          "target": "1-raw-bar"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.0169850128844686,
+          "pitch": 0.25901783699614,
+          "title": "Bath and Body",
+          "text": "<a href='https://pa.primeholistic.shop/shop/bath-and-body/16'>Shop Now</a>"
+        },
+        {
+          "yaw": -2.4250416693258785,
+          "pitch": 0.25446838143188444,
+          "title": "Hemp Flower",
+          "text": "<a href='https://pa.primeholistic.shop/shop/hemp-flower/10'>Shop Now</a>"
+        },
+        {
+          "yaw": -2.7985763469559704,
+          "pitch": 0.0005742315788275221,
+          "title": "Beverages",
+          "text": "<a href='https://pa.primeholistic.shop/shop/r-a-w-bar/6'>Shop Now</a>"
+        },
+        {
+          "yaw": 0.167406027716277,
+          "pitch": -0.07981569978288228,
+          "title": "Custom Trays",
+          "text": "L & K Custom Trays"
+        }
+      ]
+    },
+    {
+      "id": "3-pet-and-other-products",
+      "name": "Pet and Other Products",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.9106192779238134,
+        "pitch": -0.014580117198114806,
+        "fov": 1.3136650481270435
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7831874679645,
+          "pitch": -0.046219389143903555,
+          "rotation": 0,
+          "target": "4-prime-holistic-club"
+        },
+        {
+          "yaw": -1.56072776055845,
+          "pitch": 0.04972981575854618,
+          "rotation": 0,
+          "target": "2-tinctures-flower-bath-and-body"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.975778441685984,
+          "pitch": 0,
+          "title": "Bob Marley",
+          "text": "Art by Tee Catalano"
+        },
+        {
+          "yaw": 2.556672800618675,
+          "pitch": -0.07126920002614057,
+          "title": "Custom Trays",
+          "text": "L & K Custom Trays"
+        },
+        {
+          "yaw": 1.6955221761491517,
+          "pitch": 0.2512263248593971,
+          "title": "Pet Products",
+          "text": "<a href='https://pa.primeholistic.shop/shop/pet-products/13'>Shop Now</a>"
+        }
+      ]
+    },
+    {
+      "id": "4-prime-holistic-club",
+      "name": "Prime Holistic Club",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.821090501043294,
+        "pitch": 0.06853696815417187,
+        "fov": 1.3136650481270435
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 2.14619553493311,
+          "pitch": -0.24602213175771936,
+          "title": "Marley",
+          "text": "Art by Tee Catalano"
+        },
+        {
+          "yaw": 2.7938067762417456,
+          "pitch": 0.07392742085874104,
+          "title": "Prime Holistic Lounge",
+          "text": "<a href='https://primeholistic.club'>Learn More</a>"
+        }
+      ]
+    }
+  ],
+  "name": "Prime Holistic Shop Norristown",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
